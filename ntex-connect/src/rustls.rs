61ac47b1f7f7ef3a5b49b08b87d6b9f7a57fa6bd
msga-mmm <michael.sga.mmm@gmail.com>
2023-09-07 20:25:26 -0500

    feat(ntex-connect): implement clippy suggestions

--- ntex-connect/src/rustls.rs
+++ ntex-connect/src/rustls.rs
@@ -131,7 +131,7 @@ mod tests {
         });
 
         let mut cert_store = RootCertStore::empty();
-        cert_store.add_server_trust_anchors(webpki_roots::TLS_SERVER_ROOTS.0.iter().map(
+        cert_store.add_trust_anchors(webpki_roots::TLS_SERVER_ROOTS.0.iter().map(
             |ta| {
                 OwnedTrustAnchor::from_subject_spki_name_constraints(
                     ta.subject,
