b8e4700064a5eb46e8ec625967679a59e6109eb8
msga-mmm <michael.sga.mmm@gmail.com>
2023-09-07 20:30:06 -0500

    chore(ntex-connect): format code

--- ntex-connect/src/rustls.rs
+++ ntex-connect/src/rustls.rs
@@ -131,15 +131,13 @@ mod tests {
         });
 
         let mut cert_store = RootCertStore::empty();
-        cert_store.add_trust_anchors(webpki_roots::TLS_SERVER_ROOTS.0.iter().map(
-            |ta| {
-                OwnedTrustAnchor::from_subject_spki_name_constraints(
-                    ta.subject,
-                    ta.spki,
-                    ta.name_constraints,
-                )
-            },
-        ));
+        cert_store.add_trust_anchors(webpki_roots::TLS_SERVER_ROOTS.0.iter().map(|ta| {
+            OwnedTrustAnchor::from_subject_spki_name_constraints(
+                ta.subject,
+                ta.spki,
+                ta.name_constraints,
+            )
+        }));
         let config = ClientConfig::builder()
             .with_safe_defaults()
             .with_root_certificates(cert_store)
